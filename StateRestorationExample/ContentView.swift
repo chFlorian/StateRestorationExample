4913d6afc8f25da079e6f1dcc1d4f890737b773b
Florian Schweizer <post.an.flo@gmail.com>
2022-10-27 15:21:37 +0200

    Add example project

--- StateRestorationExample/ContentView.swift
+++ StateRestorationExample/ContentView.swift
@@ -2,15 +2,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum NavigationPage: Codable, Hashable {
+    case person(name: String)
+    case plant(name: String)
+}
+
+// Credit: https://nilcoalescing.com/blog/SaveCustomCodableTypesInAppStorageOrSceneStorage/
+extension [NavigationPage]: RawRepresentable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+            let result = try? JSONDecoder().decode([NavigationPage].self, from: data)
+        else {
+            return nil
         }
-        .padding()
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+            let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
+
+struct ContentView: View {
+    @AppStorage("navigationPath") var navigationPath: [NavigationPage] = []
+    
+    var body: some View {
+        NavigationStack(path: $navigationPath) {
+            List {
+                NavigationLink("Flo", value: NavigationPage.person(name: "Flo"))
+                NavigationLink("Tree", value: NavigationPage.plant(name: "Tree"))
+            }
+            .navigationDestination(for: NavigationPage.self) { page in
+                switch page {
+                    case .person(let name):
+                        Text("Person: \(name)")
+                        
+                    case .plant(let name):
+                        Text("Plant: \(name)")
+                }
+            }
+        }
     }
 }
 
